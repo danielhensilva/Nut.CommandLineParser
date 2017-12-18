8c8972a84f5a80109611475a11dafedf76ed338b
Daniel H da Silva <danielhensilva@gmail.com>
2017-12-18 17:10:21 +0000

    unit tests

---
+++ test/Exceptions/UnboundTokenExceptionTest.cs
@@ -0,0 +1,44 @@
+using System;
+using FluentAssertions;
+using Nut.CommandLineParser.Exceptions;
+using Xunit;
+
+namespace Nut.CommandLineParser.Exceptions.Test
+{ 
+    public class UnboundTokenExceptionTest
+    {
+        [Fact]
+        public void ConstructorShouldThrowExceptionForNullToken()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() =>
+                new UnboundTokenException(null)
+            );
+            exception.ParamName.Should().Be("token");
+            exception.Message.Should().Be("Value cannot be null.\r\nParameter name: token");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("  ")]
+        public void ConstructorShouldThrowExceptionForEmptyOrWhitespaceToken(string token)
+        {
+            var exception = Assert.Throws<ArgumentException>(() =>
+                new UnboundTokenException(token)
+            );
+            exception.ParamName.Should().Be("token");
+            exception.Message.Should().Be("Value cannot be empty.\r\nParameter name: token");
+        }
+
+        [Theory]
+        [InlineData("x", "Unbound token x.")]
+        [InlineData("test", "Unbound token test.")]
+        [InlineData("something wrong", "Unbound token something wrong.")]
+        public void MessagePropertyShouldHaveHelpfulMessage(string token, string exceptedMessage) 
+        {
+            var exception = new UnboundTokenException(token);
+            exception.Token.Should().Be(token);
+            exception.Message.Should().Be(exceptedMessage);
+        }
+    }
+}
