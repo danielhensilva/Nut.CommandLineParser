ac800b5a02f89065bc97a0289c5df0609aa34e25
Daniel H da Silva <danielhensilva@gmail.com>
2017-12-21 18:26:43 +0000

    unit tests

---
+++ test/Models/ArgKeyValuePairTest.cs
@@ -0,0 +1,40 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Nut.CommandLineParser.Models.Test
+{
+    public class ArgKeyValuePairTest
+    {
+        [Fact]
+        public void PropertyKeyShouldThrowExceptionForNull() 
+        {
+            string key = null;
+
+            var exception = Assert.Throws<ArgumentNullException>(() => 
+                new ArgKeyValuePair(key, "value")
+            );
+
+            exception.ParamName.Should().Be("key");
+            exception.Message.Should().Be("Value cannot be null.\r\nParameter name: key");
+        }
+
+        [Theory]
+        [InlineData("key", "value")]
+        [InlineData("alpha", "beta")]
+        public void PropertiesShouldHaveGettersAndSetters(string key, string value) 
+        {
+            var pair = new ArgKeyValuePair(key, value);
+            pair.Key.Should().Be(key);
+            pair.Value.Should().Be(value);
+
+            var updatedKey = string.Concat(key, "updated");
+            pair.Key = updatedKey;
+            pair.Key.Should().Be(updatedKey);
+
+            var updatedValue = string.Concat(value, "updated");
+            pair.Value = updatedValue;
+            pair.Value.Should().Be(updatedValue);
+        }
+    }
+}
