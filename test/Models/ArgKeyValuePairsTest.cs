c5130c0dbe0e40e6145363017e08d9df52589537
Daniel H da Silva <danielhensilva@gmail.com>
2017-12-22 10:56:56 +0000

    add ArgKeyValuePairs unit tests

--- test/Models/ArgKeyValuePairsTest.cs
+++ test/Models/ArgKeyValuePairsTest.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using Xunit;
 
@@ -6,6 +9,34 @@ namespace Nut.CommandLineParser.Models.Test
 {
     public class ArgKeyValuePairsTest
     {
-        
+        [Fact]
+        public void ParameterlessConstructorShouldInitializeEmptyCollection()
+        {
+            var pairs = new ArgKeyValuePairs();
+            pairs.Count.Should().Be(0);
+            pairs.Any().Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(new[] {"alpha", "bravo", "charlie"}, new[] {"delta", "echo", "falcon"})]
+        [InlineData(new[] {"alpha", "bravo", "charlie"}, new[] {"delta", "echo", "falcon"})]
+        public void CollectionConstructorShouldInitializeWithGivenCollection(string[] keys, string[] values)
+        {
+            var collection = new List<ArgKeyValuePair>();
+            for (var i = 0; i < keys.Length; i++)
+                collection.Add(new ArgKeyValuePair(keys[i], values[i]));
+
+            var pairs = new ArgKeyValuePairs(collection);
+            pairs.Count.Should().Be(collection.Count);
+            pairs.Any().Should().BeTrue();
+            
+            for (var i = 0; i < pairs.Count; i++)
+            {
+                var pair = pairs[i];
+                pair.Key.Should().Be(keys[i]);
+                pair.Value.Should().Be(values[i]);
+                pair.Should().BeSameAs(collection[i]);
+            }
+        }
     }
 }
